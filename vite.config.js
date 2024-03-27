f290c945ac26502471c97d4256c07270eb2009f8
abhijatain <abhi31jatain@gmail.com>
2024-03-27 17:01:27 +0530

    2nd

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,5 @@
 import { fileURLToPath, URL } from 'node:url'
-
+import { VitePWA } from 'vite-plugin-pwa'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
@@ -7,6 +7,28 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig({
   plugins: [
     vue(),
+    VitePWA(
+      { 
+        registerType: 'autoUpdate',
+        manifest: {
+          name: 'My Awesome App',
+          short_name: 'MyApp',
+          description: 'My Awesome App description',
+          theme_color: '#ffffff',
+          icons: [
+            {
+              src: 'pwa-192x192.png',
+              sizes: '192x192',
+              type: 'image/png'
+            },
+            {
+              src: 'pwa-512x512.png',
+              sizes: '512x512',
+              type: 'image/png'
+            }
+          ]
+        }
+      })
   ],
   resolve: {
     alias: {
