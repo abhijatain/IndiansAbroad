d17d9eeb721d3bb06b06ba9a2252ff362458e449
abhijatain <abhi31jatain@gmail.com>
2024-03-27 10:23:55 +0530

    2nd

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -20,6 +20,7 @@ function search() {
   }
 }
 
+
 function lightTheme() {  
     darkMode.value = !darkMode.value
     let bodyTag = document.querySelector('body');
@@ -30,6 +31,14 @@ function darkTheme() {
     let bodyTag = document.querySelector('body');
     bodyTag.dataset.bsTheme = 'dark'
 }
+
+function changeTheme() {
+  if (darkMode.value){
+    lightTheme()
+  }else{
+    darkTheme()
+  }
+}
 </script>
 
 <template>
@@ -37,8 +46,14 @@ function darkTheme() {
     <div class="container-fluid d-grid gap-3 align-items-center" style="grid-template-columns: 1fr 2fr;">
       <div >
         <div class="flex-shrink-0 ">
-            <i v-if='darkMode' class="fa-solid fa-moon fa-xl" @click="lightTheme"></i>
-            <i v-else class="fa-solid fa-sun fa-xl" @click="darkTheme"></i>
+            <div>
+              <input type="checkbox" class="checkbox" id="checkbox" @click="changeTheme">
+              <label for="checkbox" class="checkbox-label">
+                <i class="fas fa-moon"></i>
+                <i class="fas fa-sun"></i>
+                <span class="ball"></span>
+              </label>
+            </div>
         </div>
        
       </div>
@@ -103,3 +118,41 @@ function darkTheme() {
 
 </template>
 
+<style scoped>
+.checkbox {
+  opacity: 0;
+  position: absolute;
+}
+
+.checkbox-label {
+  background-color: #1679a0;
+  width: 50px;
+  height: 26px;
+  border-radius: 50px;
+  position: relative;
+  padding: 5px;
+  cursor: pointer;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+.fa-moon {color: #000000;}
+
+.fa-sun {color: #f2f0ee;}
+
+.checkbox-label .ball {
+  background-color: #fff;
+  width: 22px;
+  height: 22px;
+  position: absolute;
+  left: 2px;
+  top: 2px;
+  border-radius: 50%;
+  transition: transform 0.2s linear;
+}
+
+.checkbox:checked + .checkbox-label .ball {
+  transform: translateX(24px);
+}
+</style>
