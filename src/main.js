237938998ca8556bb1eef7b68a86529267e75144
abhijatain <abhi31jatain@gmail.com>
2024-03-28 03:43:09 +0530

    2nd

--- src/main.js
+++ src/main.js
@@ -5,17 +5,7 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
-if ('serviceWorker' in navigator) {
-    window.addEventListener('load', () => {
-      navigator.serviceWorker.register('/sw.js')
-        .then(registration => {
-          console.log('Service Worker registered:', registration);
-        })
-        .catch(error => {
-          console.error('Service Worker registration failed:', error);
-        });
-    });
-  }
+
 
 const app = createApp(App)
 
