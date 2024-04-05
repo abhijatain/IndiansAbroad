ccc21b3876f843ff831515d8dc8041aae156734d
abhijatain <abhi31jatain@gmail.com>
2024-04-05 10:23:24 +0530

    working add

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/IndiansAbroad/sw.js', { scope: '/IndiansAbroad/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
