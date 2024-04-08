2b218e8e6e39725adc3e6cca26b410d0eeffed39
abhijatain <abhi31jatain@gmail.com>
2024-04-08 21:00:23 +0530

    working add

--- assets/virtualscroller.esm--4rPf0xP.js
+++ assets/virtualscroller.esm--4rPf0xP.js
@@ -1,4 +1,4 @@
-import{B as W,s as ot,O as C,o as V,c as A,m as B,f as G,D as b,r as ct,g as M,h as Z,l as X,p as J,i as dt}from"./index-BCwL7XDl.js";var ft=`
+import{B as W,s as ot,O as C,o as V,c as A,m as B,f as G,D as b,r as ct,g as M,h as Z,l as X,p as J,i as dt}from"./index-CBjbQd10.js";var ft=`
 .p-icon {
     display: inline-block;
 }
